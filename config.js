a58459b67725b3f8dac7c9b437668247305e644e
pharmaplussystem <pharmaplussystem@gmail.com>
2026-09-06 09:01:16 -0700

    Update config.js

--- config.js
+++ config.js
@@ -10,5 +10,5 @@
   Use the browser-safe Publishable key (sb_publishable_...) or legacy anon key.
   NEVER use a secret/service_role key in this file.
 */
-window.PHARMAPLUS_SUPABASE_URL = "https://vzgpsevsebknykrzydeu.supabase.co/rest/v1/";
+window.PHARMAPLUS_SUPABASE_URL = "https://vzgpsevsebknykrzydeu.supabase.co";
 window.PHARMAPLUS_SUPABASE_KEY = "sb_publishable_D-3oJY2oBezjQ4ui1Is_nA_GEqKoqBK";
